40f01704abc6a87aa89ab1d6d5de80e98496e9e5
Yash <116657771+hackice20@users.noreply.github.com>
2025-01-22 20:06:03 +0530

    Update page.tsx

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -6,11 +6,11 @@ export default function Experience() {
           <h1 className="text-4xl font-bold">experience</h1>
           <ul className="list-disc list-inside space-y-1 text-muted-foreground">
             <li>
-              winter intern Magnacamz Technologies Pvt. Ltd.
+              <span className="text-lg font-bold">winter intern Magnacamz Technologies Pvt. Ltd.</span>
               <ul className="list-disc list-inside pl-5 space-y-1">
-                <li>Developed and maintained scalable full-stack applications using modern technologies such as React, Node.js, Express, TypeScript, and MongoDB.</li>
-                <li>Designed and deployed responsive, user-friendly interfaces with frameworks like Tailwind CSS and Vite, ensuring cross-browser and mobile compatibility.</li>
-                <li>Collaborated with diverse tools and frameworks, achieving optimized performance, security, and usability across all projects.</li>
+                <li className="italic">Developed and maintained scalable full-stack applications using modern technologies such as React, Node.js, Express, TypeScript, and MongoDB.</li>
+                <li className="italic">Designed and deployed responsive, user-friendly interfaces with frameworks like Tailwind CSS and Vite, ensuring cross-browser and mobile compatibility.</li>
+                <li className="italic">Collaborated with diverse tools and frameworks, achieving optimized performance, security, and usability across all projects.</li>
               </ul>
             </li>
           </ul>
