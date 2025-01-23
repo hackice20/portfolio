250941788cd0d1a5e679b58c47b28154b09e798b
Yash <116657771+hackice20@users.noreply.github.com>
2025-01-23 17:06:10 +0530

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,38 +1,40 @@
 "use client"
 
 import Link from "next/link"
-
+import { ModeToggle } from "@/components/mode-toggle" // Import your toggle component
 
 export function Navbar() {
   return (
     <nav className="sticky top-0 z-40 w-full border-b bg-background p-4">
-      <ul className="flex items-center gap-8 text-lg">
-        <li>
-          <Link href="/" className="font-bold">
-            hackice20
-          </Link>
-        </li>
-        <li>
-          <Link href="/projects">
-            [Projects]
-          </Link>
-        </li>
-        <li>
-          <Link href="/experience">
-            [Experience]
-          </Link>
-        </li>
-        <li>
-          <Link href="/blogs">
-            [Blogs]
-          </Link>
-        </li>
-        <li>
-        <Link href ="https://drive.google.com/file/d/1unRqggYMSWB2uhlvFay_2PE958bMi2Jg/view?usp=sharing">
-        [view resume]
-        </Link>
-        </li>
-      </ul>
+      <div className="flex items-center justify-between">
+        {/* Left side: Navbar Links */}
+        <ul className="flex items-center gap-8 text-lg">
+          <li>
+            <Link href="/" className="font-bold">
+              hackice20
+            </Link>
+          </li>
+          <li>
+            <Link href="/projects">[Projects]</Link>
+          </li>
+          <li>
+            <Link href="/experience">[Experience]</Link>
+          </li>
+          <li>
+            <Link href="/blogs">[Blogs]</Link>
+          </li>
+          <li>
+            <Link href="https://drive.google.com/file/d/1unRqggYMSWB2uhlvFay_2PE958bMi2Jg/view?usp=sharing">
+              [view resume]
+            </Link>
+          </li>
+        </ul>
+
+        {/* Right side: Toggle */}
+        <div className="shrink-0">
+          <ModeToggle />
+        </div>
+      </div>
     </nav>
   )
 }
