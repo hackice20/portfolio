3472b9e0d8144bb4391dfb60a7d6d2c9ca769c5e
Yash <116657771+hackice20@users.noreply.github.com>
2025-01-23 18:12:47 +0530

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,37 +5,28 @@ import { ModeToggle } from "@/components/mode-toggle"
 
 export function Navbar() {
   return (
-    <nav className="sticky top-0 z-40 w-full border-b bg-background p-4">
-      <ul className="flex items-center gap-8 text-lg">
-        <li>
-          <Link href="/" className="font-bold">
+    <nav className="sticky top-0 z-40 w-full border-b bg-background">
+      <div className="container mx-auto px-4 py-3 flex items-center justify-between">
+        <div className="flex items-center gap-8">
+          <Link href="/" className="font-bold text-lg">
             hackice20
           </Link>
-        </li>
-        <li>
-          <Link href="/projects">
-            [Projects]
-          </Link>
-        </li>
-        <li>
-          <Link href="/experience">
-            [Experience]
-          </Link>
-        </li>
-        <li>
-          <Link href="/blogs">
-            [Blogs]
-          </Link>
-        </li>
-        <li>
-        <Link href ="https://drive.google.com/file/d/1unRqggYMSWB2uhlvFay_2PE958bMi2Jg/view?usp=sharing">
-        [view resume]
-        </Link>
-        </li>
-        <li className="ml-auto">
-          <ModeToggle />
-        </li>
-      </ul>
+          <div className="hidden md:flex space-x-4">
+            <Link href="/projects" className="hover:underline">Projects</Link>
+            <Link href="/experience" className="hover:underline">Experience</Link>
+            <Link href="/blogs" className="hover:underline">Blogs</Link>
+            <Link 
+              href="https://drive.google.com/file/d/1unRqggYMSWB2uhlvFay_2PE958bMi2Jg/view?usp=sharing" 
+              target="_blank" 
+              rel="noopener noreferrer"
+              className="hover:underline"
+            >
+              View Resume
+            </Link>
+          </div>
+        </div>
+        <ModeToggle />
+      </div>
     </nav>
   )
 }
