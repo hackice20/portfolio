c5d6b238867be739996aa2dfdefc9b7733c510f9
Yash <116657771+hackice20@users.noreply.github.com>
2025-01-23 19:16:15 +0530

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,34 +1,37 @@
-"use client"
-
-import Link from "next/link"
-import { ModeToggle } from "@/components/mode-toggle"
-
 export function Navbar() {
   return (
-    <nav className="sticky top-0 z-40 w-full border-b bg-background">
-      <div className="container mx-auto px-4 py-3 flex items-center justify-between">
-        <div className="flex items-center gap-8">
-          <Link href="/" className="font-bold text-xl">
+    <nav className="sticky top-0 z-40 w-full border-b bg-background p-4">
+      <ul className="flex items-center gap-8 text-lg">
+        <li>
+          <Link href="/" className="font-bold">
             hackice20
           </Link>
-          <ul className="hidden md:flex items-center space-x-4">
-            <li><Link href="/projects" className="text-black dark:text-white">Projects</Link></li>
-            <li><Link href="/experience" className="text-black dark:text-white">Experience</Link></li>
-            <li><Link href="/blogs" className="text-black dark:text-white">Blogs</Link></li>
-            <li>
-              <Link 
-                href="https://drive.google.com/file/d/1unRqggYMSWB2uhlvFay_2PE958bMi2Jg/view?usp=sharing" 
-                target="_blank" 
-                rel="noopener noreferrer"
-                className="text-black dark:text-white"
-              >
-                View Resume
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <ModeToggle />
-      </div>
+        </li>
+        <li>
+          <Link href="/projects">
+            [Projects]
+          </Link>
+        </li>
+        <li>
+          <Link href="/experience">
+            [Experience]
+          </Link>
+        </li>
+        <li>
+          <Link href="/blogs">
+            [Blogs]
+          </Link>
+        </li>
+        <li classname="ml-auto">
+        <li>
+        <Link href ="https://drive.google.com/file/d/1unRqggYMSWB2uhlvFay_2PE958bMi2Jg/view?usp=sharing">
+        [view resume]
+        </Link>
+        </li>
+        <li className="ml-auto">
+          <ModeToggle />
+        </li>
+      </ul>
     </nav>
   )
 }
