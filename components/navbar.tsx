d320249cf6ee0defdc50cb3c57a3e5b850fa63be
Yash <116657771+hackice20@users.noreply.github.com>
2025-01-24 12:42:43 +0530

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,35 +1,25 @@
+// Navbar.tsx
 "use client"
-
 import Link from "next/link"
 
 export function Navbar() {
   return (
-    <nav className="sticky top-0 z-40 w-full border-b bg-background p-4">
-      <ul className="flex items-center gap-8 text-lg">
+    <nav className="sticky top-0 z-40 w-full border-b bg-background p-4 pr-14 md:pr-4">
+      <ul className="flex items-center gap-4 md:gap-8 text-lg">
         <li>
           <Link href="/" className="font-bold">
             hackice20
           </Link>
         </li>
-       
         <li>
-          <Link href="/projects">
-           [Projects]
-          </Link>
+          <Link href="/projects">[Projects]</Link>
         </li>
-
-         <li>
-          <Link href="/experience">
-           [Experience]
-          </Link>
-        </li>
-       
         <li>
-          <Link href="/blogs">
-            [Blogs]
-          </Link>
+          <Link href="/experience">[Experience]</Link>
         </li>
-
+        <li>
+          <Link href="/blogs">[Blogs]</Link>
+        </li>
       </ul>
     </nav>
   )
