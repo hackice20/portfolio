543603453a83f5b71069302d6695aa503b1b42ba
Yash <116657771+hackice20@users.noreply.github.com>
2025-02-06 18:53:25 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,15 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'hackice20',
   description: 'Personal portfolio website',
-}
+   icons: {
+    icon: [
+      { url: '/favicon.ico', type: 'image/x-icon' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    apple: '/apple-touch-icon.png',
+  },
+};
 
 export default function RootLayout({
   children,
